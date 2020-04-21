dd3f2c6c226b17831a0b00d3eff5bcf2475540e1
George Kraychik <george.kraychik@gmail.com>
2020-04-21 22:04:24 +0300

    Bug fix

--- routes/trailer/get.js
+++ routes/trailer/get.js
@@ -6,7 +6,7 @@ router.get('/', async (req, res) => {
     const session_id = req.auth_info.session_id;
 
     try {
-        const db = await mQuery(`select st.session_trailer_id, st.session_id, t.trailer_id, t.trailer_external_id, t.trailer_status, t.trailer_created_dt
+        const db = await mQuery(`select st.session_trailer_id, st.session_id, t.trailer_id, t.trailer_external_id, st.session_trailer_status
             from session_trailer st left join trailer t on st.trailer_id = t.trailer_id where st.session_id = ?`, [session_id]);
         return res.status(200).send(makeResponse(0, db));
     } catch (err) {
