2c25fe0ccd51254d8a56fbc55bff0068428b3f66
George Kraychik <george.kraychik@gmail.com>
2020-04-28 00:29:38 +0300

    Method '/user/get-last-signature' implemented

--- routes/user.js
+++ routes/user.js
@@ -7,10 +7,12 @@ const infoRouter = require('./user/info');
 const vehiclesRouter = require('./user/today-vehicles');
 const uploadSignatureRouter = require('./user/upload-signature');
 const getSignatureRouter = require('./user/get-signature');
+const getLastSignaturesRouter = require('./user/get-last-signatures');
 
 router.use('/info', infoRouter);
 router.use('/today-vehicles', vehiclesRouter);
 router.use('/upload-signature', uploadSignatureRouter);
 router.use('/get-signature', getSignatureRouter);
+router.use('/get-last-signatures', getLastSignaturesRouter);
 
 module.exports = router;
---
+++ routes/user/get-last-signatures.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const { mQuery, makeResponse } = require('../../utils');
+
+router.get('/', async (req, res) => {
+    const req_user_id = req.auth_info.req_user_id;
+
+    let db;
+    try {
+        // const query = `select date(signature_dt) as signature_day, signature_dt from signature where signature_user_id = ? and signature_dt >= date_sub(current_timestamp, interval 14 day) group by signature_day order by signature_day desc`;
+        db = await mQuery(`select signature_id, signature_dt, date_format(signature_dt, '%Y-%m-%d') as signature_day 
+            from signature where signature_user_id = ? and signature_dt >= date_sub(current_timestamp, interval 14 day) 
+            and signature_status = 'ACTIVE' order by signature_dt desc`,[req_user_id]);
+    } catch (err) {
+        return res.status(500).send(makeResponse(1, err));
+    }
+
+    return res.status(200).send(makeResponse(0, db));
+});
+
+module.exports = router;
