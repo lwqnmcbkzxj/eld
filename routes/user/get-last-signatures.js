da4cecb4821a9384804050c7d4c02223719f8a1c
George Kraychik <george.kraychik@gmail.com>
2020-05-01 21:34:16 +0300

    Bug fixed - 'REGULAR' signatures only

--- routes/user/get-last-signatures.js
+++ routes/user/get-last-signatures.js
@@ -9,7 +9,7 @@ router.get('/', async (req, res) => {
         // const query = `select date(signature_dt) as signature_day, signature_dt from signature where signature_user_id = ? and signature_dt >= date_sub(current_timestamp, interval 14 day) group by signature_day order by signature_day desc`;
         db = await mQuery(`select signature_id, signature_dt, date_format(signature_dt, '%Y-%m-%d') as signature_day 
             from signature where signature_user_id = ? and signature_dt >= date_sub(current_timestamp, interval 14 day) 
-            and signature_status = 'ACTIVE' order by signature_dt desc`,[req_user_id]);
+            and signature_status = 'ACTIVE' and signature_type = 'REGULAR' order by signature_dt desc`,[req_user_id]);
     } catch (err) {
         return res.status(500).send(makeResponse(1, err));
     }
