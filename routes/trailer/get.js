7da99fd429c8c47a8202b9f0fa371b665a46144f
George Kraychik <george.kraychik@gmail.com>
2020-05-01 23:56:56 +0300

    Added method '/trailer/get/:date'

--- routes/trailer/get.js
+++ routes/trailer/get.js
@@ -16,5 +16,22 @@ router.get('/', async (req, res) => {
 
 });
 
+router.get('/:date', async (req, res) => {
+    const req_user_id = req.auth_info.req_user_id;
+    const date = req.params.date;
+    // const session_id = req.auth_info.session_id;
+
+    try {
+        const db = await mQuery(`select st.session_trailer_id, st.session_id, t.trailer_id, t.trailer_external_id, st.session_trailer_status
+                from session_trailer st left join trailer t on st.trailer_id = t.trailer_id left join session s on st.session_id = s.session_id
+                where s.driver_user_id = ? and 
+                (date_format(st.session_trailer_dt, '%Y-%m-%d') = ?)`, [ req_user_id, date ]);
+        return res.status(200).send(makeResponse(0, db));
+    } catch (err) {
+        return res.status(500).send(makeResponse(1, err));
+    }
+
+});
+
 
 module.exports = router;
