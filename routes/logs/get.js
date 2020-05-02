2d93f6f3355434025426703b56202d734fe1fb19
George Kraychik <george.kraychik@gmail.com>
2020-05-02 20:29:31 +0300

    Calculation bug fix

--- routes/logs/get.js
+++ routes/logs/get.js
@@ -31,8 +31,8 @@ router.get('/:date', async (req, res) => {
     }
 
     try {
-        db = await mQuery(`select record_id, record_type, unix_timestamp(record_start_dt) as record_start_dt, unix_timestamp(record_end_dt) as record_end_dt 
-            from record where record_status <> 'DELETED' and driver_user_id = ? and record_type in ('DRIVING', 'ON_DUTY', 'ON_DUTY_YM') and 
+        db = await mQuery(`select record_id, record_type in ('ON_DUTY', 'DRIVING', 'ON_DUTY_YM') as record_type_ok, unix_timestamp(record_start_dt) as record_start_dt, unix_timestamp(record_end_dt) as record_end_dt 
+            from record where record_status <> 'DELETED' and driver_user_id = ? and 
             (((date_format(record_start_dt, '%Y-%m-%d') <= ?) and (date_format(record_end_dt, '%Y-%m-%d') >= ?)) or 
              ((date_format(record_start_dt, '%Y-%m-%d') <= ?) and (record_end_dt is null)))`, [req_user_id, date, date, date]);
     } catch (err) {
@@ -40,10 +40,14 @@ router.get('/:date', async (req, res) => {
     }
 
     const cur_dt = Math.round(Date.now() / 1000);
-    const delta_time = db.map(rec => {
-        if (rec.record_end_dt == null) return cur_dt - rec.record_start_dt;
-        else return rec.record_end_dt - rec.record_start_dt;
-    }).reduce((a, b) => a + b, 0);
+    let delta_time = 0;
+    const n = db.length;
+    for (let i = 0; i < n; i++) {
+        if (db[i].record_type_ok === 1) {
+            if (i + 1 >= n) delta_time += cur_dt - db[i].record_start_dt;
+            else delta_time += db[i+1].record_start_dt - db[i].record_start_dt;
+        }
+    }
 
     return res.status(200).send(makeResponse(0, {
         has_inspection: has_inspection ? 1 : 0,
