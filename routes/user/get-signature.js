b739d4f1a487fd7437e1b437d3232298df579d6d
George Kraychik <george.kraychik@gmail.com>
2020-05-02 21:11:52 +0300

    Method '/user/get-signature/<date>' implemented

--- routes/user/get-signature.js
+++ routes/user/get-signature.js
@@ -19,4 +19,22 @@ router.get('/', async (req, res) => {
     // return res.send(makeResponse(0, file_path));
 });
 
+router.get('/:date', async (req, res) => {
+    const req_user_id = req.auth_info.req_user_id;
+    const date = req.params.date;
+
+    // find link to last signature
+    let db;
+    try {
+        db = await mQuery(`select signature_src from signature where signature_user_id = ? and signature_type = 'REGULAR'
+            and date_format(signature_dt, '%Y-%m-%d') = ? order by signature_dt desc limit 1`, [ req_user_id, date ]);
+    } catch (err) {
+        return res.status(500).send(makeResponse(1, err));
+    }
+    if (db.length <= 0) return res.status(404).send(makeResponse(2, 'No signature found'));
+    const file_path = db[0].signature_src;
+    return res.download(file_path);
+    // return res.send(makeResponse(0, file_path));
+});
+
 module.exports = router;
