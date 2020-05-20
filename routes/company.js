38268bf020e94c82bda147c47f1f4534bd85f0b0
George Kraychik <george.kraychik@gmail.com>
2020-05-20 21:51:14 +0300

    Method PATCH '/company/deactivate/<company_id>' implemented

--- routes/company.js
+++ routes/company.js
@@ -15,6 +15,7 @@ const driversRouter = require('./company/drivers');
 const terminalsRouter = require('./company/terminals');
 const vehiclesRouter = require('./company/vehicles');
 const activateRouter = require('./company/activate');
+const deactivateRouter = require('./company/deactivate');
 
 router.use('/info', infoRouter);
 router.use('/add', addRouter);
@@ -23,6 +24,7 @@ router.use('/drivers', driversRouter);
 router.use('/terminals', terminalsRouter);
 router.use('/vehicles', vehiclesRouter);
 router.use('/activate', activateRouter);
+router.use('/deactivate', deactivateRouter);
 
 
 module.exports = router;
