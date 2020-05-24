fd2ac81c857e7a3d03561854cca1531cafb28ac5
George Kraychik <george.kraychik@gmail.com>
2020-05-24 11:05:18 +0300

    Updated

--- routes/company/drivers.js
+++ routes/company/drivers.js
@@ -15,9 +15,12 @@ router.get('/:company_id', async (req, res) => {
     }
 
     try {
-        db = await mQuery(`select user_id, user_first_name, user_last_name, 
-        concat(user_first_name, ' ', user_last_name) as user_full_name from user 
-        where role_id = 1 and user_status <> 'DELETED' and company_id = ?`, [ company_id ]);
+        db = await mQuery(`select u.user_id, u.user_first_name, u.user_last_name, 
+        concat(u.user_first_name, ' ', u.user_last_name) as user_full_name, u.user_login, u.user_phone,
+        v.vehicle_external_id as vehicle_truck_number, u.user_notes, u.user_status, '3.2.9' as app_version,
+       'Wireless Link/BL.02.48' as device_version
+        from user u left join vehicle v on u.default_vehicle_id = v.vehicle_id
+        where u.role_id = 1 and u.company_id = ?`, [ company_id ]);
     } catch (err) {
         return res.status(500).send(makeResponse(2, err));
     }
