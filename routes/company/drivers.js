ad73391956f0e2cd6b297fd66c7a1f74ae111079
George Kraychik <george.kraychik@gmail.com>
2020-05-24 11:27:49 +0300

    Functionality for admin updated

--- routes/company/drivers.js
+++ routes/company/drivers.js
@@ -3,10 +3,11 @@ const { mQuery, makeResponse } = require('../../utils');
 const Joi = require('@hapi/joi');
 
 router.get('/:company_id', async (req, res) => {
+    // if (company_id <= 0) then "No company filter" else "Company Filter"
     let db, company_id;
     try {
         const schema = Joi.object({
-            company_id: Joi.number().integer().min(1).required(),
+            company_id: Joi.number().integer().required(),
         });
         const vars = await schema.validateAsync(req.params);
         company_id = vars.company_id;
@@ -14,13 +15,18 @@ router.get('/:company_id', async (req, res) => {
         return res.status(400).send(makeResponse(1, err));
     }
 
+    const to_filter_company = (company_id > 0);
+    const company_filter = (to_filter_company) ? " and u.company_id = ?" : "";
     try {
-        db = await mQuery(`select u.user_id, u.user_first_name, u.user_last_name, 
+        const query = `select u.user_id, u.user_first_name, u.user_last_name, 
         concat(u.user_first_name, ' ', u.user_last_name) as user_full_name, u.user_login, u.user_phone,
         v.vehicle_external_id as vehicle_truck_number, u.user_notes, u.user_status, '3.2.9' as app_version,
        'Wireless Link/BL.02.48' as device_version
         from user u left join vehicle v on u.default_vehicle_id = v.vehicle_id
-        where u.role_id = 1 and u.company_id = ?`, [ company_id ]);
+        where u.role_id = 1` + company_filter;
+        const params = to_filter_company ? [ company_id ] : [ ];
+        console.log(params);
+        db = await mQuery(query, params);
     } catch (err) {
         return res.status(500).send(makeResponse(2, err));
     }
