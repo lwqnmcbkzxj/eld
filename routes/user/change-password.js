ae0c436a98bff13f3d83453cacf38a4fb4a40ba5
George Kraychik <george.kraychik@gmail.com>
2020-06-06 15:14:36 +0300

    Added input field 'user_id'

--- routes/user/change-password.js
+++ routes/user/change-password.js
@@ -5,10 +5,10 @@ const { mQuery, makeResponse } = require('../../utils');
 const body_parser = require('body-parser').urlencoded({ extended: false });
 
 router.patch('/', body_parser, async (req, res) => {
-    const req_user_id = req.auth_info.req_user_id;
     let vars = {}, db = null;
     try {
         const schema = Joi.object({
+            user_id: Joi.number().integer().min(1),
             old_password: Joi.string().required(),
             new_password: Joi.string().min(6).required(),
             new_password_confirmation: Joi.string().min(6).required()
@@ -18,15 +18,26 @@ router.patch('/', body_parser, async (req, res) => {
         return res.status(400).send(makeResponse(1, err));
     }
 
+    const req_user_id = (vars.user_id) ? vars.user_id : req.auth_info.req_user_id;
+
     if (vars.new_password.localeCompare(vars.new_password_confirmation)) {
         return res.status(403).send(makeResponse(4, 'Passwords are not equal'));
     }
+
+    try {
+        db = await mQuery(`select user_id from user where user_id = ?`, [ req_user_id ]);
+    } catch (err) {
+        return res.status(500).send(makeResponse(5, err));
+    }
+    if (db.length <= 0) return res.status(500).send(makeResponse(4, 'User with user_id = ' + req_user_id + ' does not exist'));
+    db = null;
 
     try {
         db = await mQuery(`select user_password from user where user_id = ?`, [ req_user_id ]);
     } catch (err) {
         return res.status(500).send(makeResponse(2, err));
     }
+
     if (db[0].user_password.localeCompare(md5(vars.old_password))) {
         return res.status(403).send(makeResponse(3, { text: 'Wrong old password received'}));
     }
@@ -39,7 +50,7 @@ router.patch('/', body_parser, async (req, res) => {
         return res.status(500).send(makeResponse(2, err));
     }
 
-    return res.status(200).send(makeResponse(0, {changedRows: db.changedRows}));
+    return res.status(200).send(makeResponse(0, {changedRows: db.changedRows, user_id: req_user_id }));
 });
 
 module.exports = router;
