f5e3be407a6d7765982289eaa06f4a97445002ca
George Kraychik <george.kraychik@gmail.com>
2020-06-13 01:40:52 +0300

    Refactoring is done from callbacks to async/await notation

--- routes/auth/login.js
+++ routes/auth/login.js
@@ -1,58 +1,71 @@
 const express = require('express');
 const router = express.Router();
 
-const { makeQuery, makeResponse } = require('../../utils.js');
+const { mQuery, makeResponse } = require('../../utils.js');
 const crypto = require('crypto');
 const md5 = require('md5');
+const Joi = require('@hapi/joi');
 
-router.post('/', function(req, res) { /* user_login, user_password */
-    // console.log(req.body);
-    const user_login = req.body.user_login;
-    const user_password = req.body.user_password;
-    if (!user_password) return res.status(400).send(makeResponse(1, 'Empty password received'));
-    if (!user_login) return res.status(400).send(makeResponse(2, 'Empty login received'));
+router.post('/', async function(req, res) { /* user_login, user_password */
+    let db_result, dres, session_db_ok, vars;
+    try {
+        const schema = Joi.object({
+            user_login: Joi.string().required(),
+            user_password: Joi.string().required(),
+            is_web: Joi.number().integer().min(0).max(1)
+        });
+        vars = await schema.validateAsync(req.body);
+    } catch (err) {
+        return res.status(400).send(makeResponse(1, err));
+    }
+    const user_login = vars.user_login;
+    const user_password = vars.user_password;
+    const is_web = (vars.is_web) ? vars.is_web : 0;
+    // if (!user_password) return res.status(400).send(makeResponse(1, 'Empty password received'));
+    // if (!user_login) return res.status(400).send(makeResponse(2, 'Empty login received'));
 
     const user_password_hashed = md5(user_password);
-    makeQuery('select user_id, user_login, user_password, role_id, company_id from user where user_login = ?',
-        [user_login], (db_result) => {
-
-        if (db_result.result.length <= 0) return res.status(404).send(makeResponse(6, 'User not found'));
-        const user_info = db_result.result[0];
-        const db_password_hash = user_info.user_password;
-        const user_id = user_info.user_id;
-        if (!db_password_hash.localeCompare(user_password_hashed)) {    // passwords match
-            const buffer = crypto.randomBytes(20);
-            const user_token = buffer.toString('hex');
-            // console.log(user_token);
-            makeQuery(`update user set user_token = ? where user_login = ?`, [user_token, user_login], (dres) => {
-                makeQuery(`select * from session where driver_user_id = ? and session_status = 'ACTIVE'`, [user_id],
-                    (session_db_ok) => {
-                        const session_len = session_db_ok.result.length;
-                        const session_id = (session_len <= 0) ? null : session_db_ok.result[0].session_id;
-                        console.log("Session ID: " + session_id);
-                        return res.header({token: user_token}).status(200).send(makeResponse(0, {
-                            login: user_login,
-                            token: user_token,
-                            user_id: user_id,
-                            role_id: user_info.role_id,
-                            company_id: user_info.company_id,
-                            session_id: session_id
-                        }));
-                    }, (session_db_err) => {
-                        return res.status(500).send(makeResponse(6, session_db_err));
-                    }
-                );
-            }, (dres) => {
-                // console.log(dres);
-                return res.status(500).send(makeResponse(5, 'Unexpected error while updating database'));
-            });
-        } else {
-            return res.status(401).send(makeResponse(4, 'Wrong login / password'));
-        }
-
-    }, (res) => {
+    try {
+        db_result = await mQuery('select user_id, user_login, user_password, role_id, company_id from user where user_login = ?',
+            [user_login]);
+    } catch (err) {
         return res.status(500).send(makeResponse(3, 'Unexpected error while executing SELECT request'));
-    });
+    }
+
+    if (db_result.length <= 0) return res.status(404).send(makeResponse(6, 'User not found'));
+    const user_info = db_result[0];
+    const db_password_hash = user_info.user_password;
+    const user_id = user_info.user_id;
+    if (db_password_hash.localeCompare(user_password_hashed)) {    // passwords not match
+        return res.status(401).send(makeResponse(4, 'Wrong login / password'));
+    }
+
+    const buffer = crypto.randomBytes(20);
+    const user_token = buffer.toString('hex');
+    try {
+        dres = await mQuery(`update user set user_token = ? where user_login = ?`, [user_token, user_login]);
+    } catch (err) {
+        return res.status(500).send(makeResponse(5, 'Unexpected error while updating database'));
+    }
+
+    try {
+        session_db_ok = await mQuery(`select * from session where driver_user_id = ? and session_status = 'ACTIVE'`, [user_id]);
+    } catch (err) {
+        return res.status(500).send(makeResponse(6, err));
+    }
+
+    const session_len = session_db_ok.length;
+    const session_id = (session_len <= 0) ? null : session_db_ok[0].session_id;
+    console.log("Session ID: " + session_id);
+    return res.header({token: user_token}).status(200).send(makeResponse(0, {
+        login: user_login,
+        token: user_token,
+        user_id: user_id,
+        role_id: user_info.role_id,
+        company_id: user_info.company_id,
+        session_id: session_id
+    }));
+
 });
 
 module.exports = router;
